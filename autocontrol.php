ac34484390ce1ed10deeea09dd12f92511d36cfd
Chris <christianto.g.14@gmail.com>
2018-05-06 21:21:28 +0700

    auto control fix

--- autocontrol.php
+++ autocontrol.php
@@ -7,7 +7,7 @@
 Class Autocontrol {
     static function boot() {
         $r_method = strtolower($_SERVER['REQUEST_METHOD']);
-        $path = explode("/", $_SERVER['REQUEST_URI']);
+        $path = explode("/", trim($_SERVER['REQUEST_URI'], "/"));
         $method_call = "index";
         if(empty($path[0]))
             $path[0] = "index";
@@ -15,7 +15,7 @@ Class Autocontrol {
             $method_call = $path[1];
         $classname = "Controller\\" . $path[0];
         $method_call = $r_method . "_" . $method_call;
-
+        
         // bentuk url yang dipengenin:
         // /controller/method/param/param/param?param_get=value
         // Controller\controller->method(param,param);
