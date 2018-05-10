0a455751d77f6a56e948ceb64b8d2d70f42b3b4c
Chris <christianto.g.14@gmail.com>
2018-05-10 13:47:07 +0700

    fix for autoloader

--- autocontrol.php
+++ autocontrol.php
@@ -7,7 +7,7 @@
 Class Autocontrol {
     static function boot() {
         $r_method = strtolower($_SERVER['REQUEST_METHOD']);
-        $path = explode("/", trim($_SERVER['REQUEST_URI'], "/"));
+        $path = explode("/", trim(self::reconstruct_url(), "/"));
         $method_call = "index";
         if(empty($path[0]))
             $path[0] = "index";
@@ -26,5 +26,14 @@ Class Autocontrol {
             call_user_func([$classname, $method_call], $param);
         }
         //done.
+    }
+
+    static function reconstruct_url($url = null){
+        if(!$url) {
+            $url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        }    
+        $url_parts = parse_url($url);
+        $constructed_url = (isset($url_parts['path'])?$url_parts['path']:'/');
+        return $constructed_url;
     }
 }
